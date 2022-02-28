edf797e9e867d2ea9fd0cf454371e115d7b0d514
Isaiah <isaiah.leiva@gmail.com>
2022-02-28 15:25:56 -0700

    commit

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -5,7 +5,11 @@ class DbContext {
   Values = mongoose.model('Value', ValueSchema);
 
 
-  Burgers = [{ id: 1, name: 'Goodburger', cheese: 'cheddar' }, { id: 2, name: 'Greatburger', cheese: 'pepperjack' }, { id: 3, name: 'Amazingburger', cheese: 'feta' }]
+  Burgers = [
+    { id: 1, name: 'Goodburger', cheese: 'cheddar' },
+    { id: 2, name: 'Greatburger', cheese: 'pepperjack' },
+    { id: 3, name: 'Amazingburger', cheese: 'feta' }
+  ]
 }
 
 export const dbContext = new DbContext()
