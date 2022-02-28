18326c52c7c24a098d8f16db70a16fc186503972
Isaiah <isaiah.leiva@gmail.com>
2022-02-28 14:18:24 -0700

    commit

--- client/app/Services/BurgersService.js
+++ client/app/Services/BurgersService.js
@@ -2,5 +2,13 @@
 
 
 class BurgersService {
-
+    removeBurger(burgerId) {
+        logger.log('burgerId form user', typeof burgerId)
+        const index = dbContext.Burgers.findIndex(b => b.id.toString() === burgerId)
+        if (index === -1) {
+            throw new Error('no burger at that id')
+        }
+        dbContext.Burgers.splice(index, 1)
+        return 'No burger fren'
+    }
 }
